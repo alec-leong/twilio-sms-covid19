36266e22323767b4f905e74b962d8216db1eb8ef
Alec Leong <leongalec@gmail.com>
2020-09-21 01:14:54 -0700

    Updated query help functions to send outbound SMS text delivery.

--- database/query.js
+++ database/query.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const twilioClient = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 const PhoneNumbers = require('./model.js');
+const port = process.env.PORT || 3000;
+const homepage = process.env.HOMEPAGE || `http://localhost:${port}/`;
 
 /**
  * Function description here.
@@ -35,8 +37,22 @@ const insertPhoneRecord = async (phoneRecord, res) => {
       // const subscriptionStatus = err.errors[0].instance.dataValues.subscription_status;
 
       if (/^pending$/i.test(existingUser.subscription_status)) {
+        twilioClient.messages
+          .create({
+            to: user.e164_format,
+            from: process.env.TWILIO_PHONE_NUMBER,
+            body: 'Reply YES to subscribe. Reply STOP to unsubscribe. Msg&Data Rates May Apply.',
+          })
+          .then((message) => console.log(message));
         res.status(500).send({ message: 'Pending subscription.' });
       } else if (/^subscribed$/i.test(existingUser.subscription_status)) {
+        twilioClient.messages
+          .create({
+            to: user.e164_format,
+            from: process.env.TWILIO_PHONE_NUMBER,
+            body: `You have successfully subscribed to messages from this number. Reply STOP to unsubscribe. Msg&Data Rates May Apply.\n\n${homepage}`,
+          })
+          .then((message) => console.log(message));
         res.status(500).send({ message: 'Already subscribed.' });
       } else {
         res.status(500).send({ message: 'An unexpected error occurred on a send.' });
