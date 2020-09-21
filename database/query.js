7edaa844334ca3705beb7e107ea459e13178e694
Alec Leong <leongalec@gmail.com>
2020-09-20 20:18:40 -0700

    Query helper functions.

---
+++ database/query.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+const twilioClient = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+const PhoneNumbers = require('./model.js');
+
+/**
+ * Function description here.
+ * @function
+ * @param {Object} phoneRecord - Description here.
+ * @param {string} phoneRecord.country_code - Description here.
+ * @param {string} phoneRecord.identification_code - Description here.
+ * @param {string} phoneRecord.subscriber_number - Description here.
+ * @param {string} phoneRecord.e164_format - Description here.
+ * @param {Object} res - Description here.
+ */
+const insertPhoneRecord = async (phoneRecord, res) => {
+  // const copyPhoneRecord = { ...phoneRecord };
+  // copyPhoneRecord.confirmation_code = md5(phoneRecord.e164_format).toString();
+
+  try {
+    const user = await PhoneNumbers.create(phoneRecord/* copyPhoneRecord */);
+    twilioClient.messages
+      .create({
+        to: user.e164_format,
+        from: process.env.TWILIO_PHONE_NUMBER,
+        body: 'Reply YES to subscribe. Reply STOP to unsubscribe. Msg&Data Rates May Apply.',
+      })
+      .then((message) => console.log(message));
+    res.send({ message: 'Pending subscription.' });
+  } catch (err) {
+    console.error(err);
+
+    if (err.name === 'SequelizeUniqueConstraintError' || err.parent.code === '23505') { // `e164_format` field must be unique.
+      const subscriptionStatus = err.errors[0].instance.dataValues.subscription_status;
+
+      if (/^pending$/i.test(subscriptionStatus)) {
+        res.status(500).send({ message: 'Pending subscription.' });
+      } else if (/^subscribed$/i.test(subscriptionStatus)) {
+        res.status(500).send({ message: 'Already subscribed.' });
+      } else {
+        res.status(500).send({ message: 'An unexpected error occurred on a send.' });
+      }
+    } else {
+      res.status(500).send({ message: `${err}` });
+    }
+  }
+};
+
+const query = {
+  insertPhoneRecord,
+};
+
+module.exports = query;
