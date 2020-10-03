4eb8f1af733871517c141585e99a01fc50f6cd0d
Alec Leong <leongalec@gmail.com>
2020-10-03 00:16:07 -0700

    Code-clean up and added code documentation to <IndexPage />.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,26 @@
 import axios from 'axios';
 import Head from 'next/head';
 import React from 'react';
-import { register, unregister } from 'next-offline/runtime';
 
+/**
+ * Creates a JSX component representing the index page.
+ * @class
+ * @member {Object} state The component's internal state.
+ * @member {Object} phoneRegex A regular expression of the US national phone number format.
+ * @member {Array} fields An array of strings where each element represents a database field.
+ * @member {function} clearForm A method to reset the component's state object.
+ * @member {function} handleChange A method to handle input change actions in the DOM.
+ * @member {function} handleSubmit A method to handle form submit action in the DOM.
+ */
 class IndexPage extends React.Component {
+  /**
+   * Constructor. Having a constructor is mandatory to invoke `super()` because `IndexPage` is a
+   * subclass of `React.Component`.
+   * @constructor
+   * @param {Object} props Stands for properties.
+   */
   constructor(props) {
+    // Invoke `super(props)`. Otherwise, accessing `this.props` will evaluate to `undefined`.
     super(props);
 
     this.phoneRegex = RegExp(/^\([0-9]{3}\)\s{1}[0-9]{3}-[0-9]{4}$/);
@@ -22,16 +38,17 @@ class IndexPage extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  /**
+   * Sets `<html lang="en">` and  `body { font-family: "Open Sans", sans-serif; }`.
+   */
   componentDidMount() {
     document.documentElement.setAttribute('lang', 'en');
     document.body.setAttribute('style', 'font-family: "Open Sans", sans-serif;');
-    register('/service-worker.js', { scope: '/' });
   }
 
-  componentWillUnmount() {
-    unregister();
-  }
-
+  /**
+   * Resets the component's state object.
+   */
   clearForm() {
     this.setState({
       phoneNumber: '',
@@ -42,6 +59,13 @@ class IndexPage extends React.Component {
     });
   }
 
+  /**
+   * Update the component's state object.
+   * @param {Object} event Represents an event triggered by the user input change action in the DOM.
+   * @param {Object} event.target A reference to the object onto which the event was dispatched.
+   * @param {string} event.target.name A reference to an HTML element's `name` attribute's value.
+   * @param {string} event.target.value A reference to an HTML element's `value` attribute's value.
+   */
   handleChange(event) {
     const { name, value } = event.target;
 
@@ -50,12 +74,25 @@ class IndexPage extends React.Component {
     });
   }
 
+  /**
+   * Prevents page refresh. Checks if reCAPTCHA token exists. If reCAPTCHA token exists then check
+   * if user phone number input is valid. If reCAPTCHA token exists and phone number is valid, send
+   * POST request to server at `/sms-form` route. Handles OK request and BAD request from server.
+   * @async
+   * @param {Object} event Represents an event triggered by the user form submit action in the DOM.
+   */
   async handleSubmit(event) {
-    // alert(grecaptcha.getResponse(/*widget*/));
+    // Prevent page refresh.
     event.preventDefault();
 
+    /**
+     * The response for the first reCAPTCHA widget created.
+     * @type {string}
+     * Note: `grecaptcha` is defined from loaded google-recaptcha CDN script.
+     */
     const captchaResponse = grecaptcha.getResponse();
 
+    // Handle empty reCAPTCHA response string.
     if (!captchaResponse) {
       this.setState({
         formMessage: {
@@ -66,12 +103,18 @@ class IndexPage extends React.Component {
       return;
     }
 
+    // US national formated phone number.
     const { phoneNumber } = this.state;
 
+    // Handle valid phone number format.
     if (this.phoneRegex.test(phoneNumber)) {
+      // Initialize `phoneRecord` object.
       const phoneRecord = { country_code: '1', subscription_status: 'pending' };
+      // Create a regular expression to find a sequence of numbers in phone number.
       const decodePhoneNumber = /\d{1,}/g;
+      // Create an array to store each sequence of numbers in phone number.
       const partitionedPhoneNumber = [];
+      // Search for a sequence of numbers in the specified string `phoneNumber`.
       let results = decodePhoneNumber.exec(phoneNumber);
       while (results) {
         partitionedPhoneNumber.push(results[0]);
@@ -85,15 +128,18 @@ class IndexPage extends React.Component {
       │    1    │ '123'  │
       │    2    │ '4567' │
       └─────────┴────────┘ */
+      // Add key-value pairs to `phoneRecord` object.
       [phoneRecord.identification_code] = [...partitionedPhoneNumber];
       phoneRecord.subscriber_number = partitionedPhoneNumber[1] + partitionedPhoneNumber[2];
       phoneRecord.e164_format = '+' + phoneRecord.country_code + phoneRecord.identification_code 
         + phoneRecord.subscriber_number;
 
+      // POST request to server. Add `phoneRecord` and captchaResponse` to `req.body`.
       axios.post('/sms-form', { phoneRecord, captchaResponse })
         .then((res) => {
           const { message } = res.data;
 
+          // Modify state to print message to webpage.
           this.setState({
             formMessage: {
               message,
@@ -102,10 +148,13 @@ class IndexPage extends React.Component {
           });
         })
         .catch((err) => {
-          console.error(err);
-          // Nested object destructuring.
+          // Remove client-side terminal logging. Instead, print error message to webpage.
+          // console.error(err);
+
+          // Nested object destructuring and aliasing.
           const { response: { data: { message } } } = err;
 
+          // Modify state to print error message to webpage.
           this.setState({
             formMessage: {
               message,
@@ -114,11 +163,14 @@ class IndexPage extends React.Component {
           });
         })
         .finally(() => {
-          grecaptcha.reset();
+          grecaptcha.reset(); // Invoke the `reset` method to reset the reCAPTCHA widget.
         });
     }
   }
 
+  /**
+   * Renders `<IndexPage />`.
+   */
   render() {
     const { phoneNumber, formMessage } = this.state;
     return (
@@ -138,16 +190,11 @@ class IndexPage extends React.Component {
           <link rel="manifest" href="/manifest.webmanifest" />
           <link rel="preconnect" href="https://fonts.googleapis.com/" crossOrigin="true" />
           <link rel="prefetch" href="https://fonts.googleapis.com/css2?family=Open+Sans" crossOrigin="true" />
-          {/* <script type="text/javascript" src="https://www.google.com/recaptcha/api.js" async defer /> */}
-          <script type="text/javascript" src="/service-worker.js" />
-          <script type="text/javascript" src="/register-sw.js" />
-          {/* <script type="text/javascript" src="/recaptcha-v2.js" /> */}
         </Head>
         <div className="sms-container">
           <fieldset>
             <legend className="header2">City of San Francisco COVID-19 SMS Text Delivery Sign-up Form</legend>
             <form className="form-container" onSubmit={this.handleSubmit}>
-            {/* <form className="form-container" onSubmit={this.handleSubmit}> */}
               <label htmlFor="phone" className="phone-label">Enter your phone number:</label>
               <input type="text" id="phone" className="phone-input" name="phoneNumber" placeholder="(123) 456-7890" pattern="\([0-9]{3}\)\s{1}[0-9]{3}-[0-9]{4}" minLength="14" maxLength="14" autoComplete="on" onChange={this.handleChange} value={phoneNumber} required />
               <input type="reset" className="clear-input" value="Clear" onClick={this.clearForm} />
@@ -156,13 +203,13 @@ class IndexPage extends React.Component {
               {formMessage.message
                 ? <p className="form-message-paragraph" style={{ color: formMessage.textColor }}>{formMessage.message}</p>
                 : ''}
-              {/* <div id="recaptcha-v2-widget" className="g-recaptcha"></div> */}
               <div className="g-recaptcha" data-sitekey={process.env.NEXT_PUBLIC_reCAPTCHA_SITE_KEY} data-theme="dark" />
               <img src="/twilio-badge-red.475897ec8.svg" className="twilio-img" alt="twilio badge" />
             </form>
           </fieldset>
           <script type="text/javascript" src="https://www.google.com/recaptcha/api.js" async defer />
-          {/* <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit" async defer /> */}
+          <script type="text/javascript" src="/service-worker.js" />
+          <script type="text/javascript" src="/register-sw.js" />
         </div>
         <style jsx>{`
           .sms-container {
