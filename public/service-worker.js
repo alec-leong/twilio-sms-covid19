4a3446ec05e561d90864bc8ed23d4fa02d911aa0
Alec Leong <leongalec@gmail.com>
2020-10-04 15:25:08 -0700

    Added service workers `start_url` and current page responds with a 200 when offline.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,31 @@
+const cacheName = 'twilio-sms-covid19-v1';
+
 self.addEventListener('install', (event) => {
   console.log('Inside the UPDATED install handler:', event);
+  event.waitUntil(
+    caches.open(cacheName)
+      .then((cache) => cache.addAll(
+        [
+          '/',
+          '/index',
+          '/twilio-badge-red.475897ec8.svg',
+        ],
+      ).then(() => self.skipWaiting())),
+  );
 });
 
 self.addEventListener('activate', (event) => {
   console.log('Inside the activate handler:', event);
+
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
   console.log('Inside the fetch handler:', event);
+
+  event.respondWith(
+    caches.open(cacheName)
+      .then((cache) => cache.match(event.request, { ignoreSearch: true }))
+      .then((response) => response || fetch(event.request)),
+  );
 });
