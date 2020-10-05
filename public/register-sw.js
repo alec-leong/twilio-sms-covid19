d6370ffad6371fe516ce5ff9cb5679019a9c89d1
Alec Leong <leongalec@gmail.com>
2020-10-04 20:18:16 -0700

    Code clean-up register service workers.

--- public/register-sw.js
+++ public/register-sw.js
@@ -1,9 +1,10 @@
 if ('serviceWorker' in navigator) { // Check whether `navigator.serviceWorker` is supported
-  window.addEventListener('load', function () {
-    navigator.serviceWorker.register('/service-worker.js', { scope: '/' }).then(function (registration) {
-      console.log('SW registered: ', registration)
-    }).catch(function (registrationError) {
-      console.log('SW registration failed: ', registrationError)
-    })
-  })
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js', { scope: '/' })
+      .then((registration) => {
+        console.log(`SW registered: ${registration}`);
+      }).catch((registrationError) => {
+        console.log(`SW registration failed: ${registrationError}`);
+      });
+  });
 }
